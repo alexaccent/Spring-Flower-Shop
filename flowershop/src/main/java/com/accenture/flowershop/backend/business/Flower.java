3c19752d71b6889554458242ab60c7c0d127a6a8
aleksandr.polyakov <alexworkaccent@gmail.com>
2019-08-13 14:51:46 +0300

    Add Spring Framework

---
+++ flowershop/src/main/java/com/accenture/flowershop/backend/business/Flower.java
@@ -0,0 +1,20 @@
+package com.accenture.flowershop.backend.business;
+
+/**
+ * @name String
+ */
+public class Flower {
+    private String name;
+
+    public Flower(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
