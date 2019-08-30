a5906c183436417366f8d6cc131b424e8d65e602
aleksandr.polyakov <alexworkaccent@gmail.com>
2019-08-30 13:32:13 +0300

    Edit business layout

--- flowershop/src/main/java/com/accenture/flowershop/backend/business/interfaces/UserAccessService.java
+++
@@ -1,8 +0,0 @@
-package com.accenture.flowershop.backend.business.interfaces;
-
-import com.accenture.flowershop.backend.entity.User;
-
-public interface UserAccessService {
-    User login(String user, String password);
-    User register(String login, String phone,String address, String password);
-}
