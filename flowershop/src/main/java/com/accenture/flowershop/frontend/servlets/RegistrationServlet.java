963e02f528ae24d8852a05e679a461dda028b1f3
aleksandr.polyakov <alexworkaccent@gmail.com>
2019-08-30 13:43:30 +0300

    Edit Registration servlet for work with services

--- flowershop/src/main/java/com/accenture/flowershop/frontend/servlets/RegistrationServlet.java
+++ flowershop/src/main/java/com/accenture/flowershop/frontend/servlets/RegistrationServlet.java
@@ -1,10 +1,12 @@
 package com.accenture.flowershop.frontend.servlets;
 
-import com.accenture.flowershop.backend.business.UserAccessServiceImpl;
-import com.accenture.flowershop.backend.business.interfaces.UserAccessService;
-import com.accenture.flowershop.backend.entity.User;
+import com.accenture.flowershop.backend.entity.Customer;
+import com.accenture.flowershop.backend.services.Impl.UserBusinessServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +18,15 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/registration")
 public class RegistrationServlet extends HttpServlet {
 
+    @Autowired
+    private UserBusinessServiceImpl userServicesRegistration;
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
+                config.getServletContext());
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -25,33 +36,35 @@ public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8");
+
         String login = req.getParameter("login");
         String phone = req.getParameter("phone");
         String address = req.getParameter("address");
         String password = req.getParameter("password");
 
         String nameJsp = "registration.jsp";
-
         if (login != null && !login.isEmpty() && password != null && !password.isEmpty() && phone != null && !phone.isEmpty() && address != null && !address.isEmpty()){
 
-            UserAccessService userAccessService = new UserAccessServiceImpl();
-            User returnUser = userAccessService.register(login, phone, address, password);
+            Customer returnUser = (Customer) userServicesRegistration.register(login, password, phone, address);
 
             if (returnUser != null) {
 
                 resp.sendRedirect("/");
             } else {
+
                 String error = "Данный логин уже занят";
                 req.setAttribute("error", error);
                 RequestDispatcher dispatcher = req.getRequestDispatcher(nameJsp);
                 dispatcher.forward(req, resp);
             }
         } else {
+
             String error = "Пожалуйста заполните все поля формы";
             req.setAttribute("error", error);
             RequestDispatcher dispatcher = req.getRequestDispatcher(nameJsp);
             dispatcher.forward(req, resp);
         }
-
     }
 }
