12f61d09f0386e28465df1b3eacbb40043743d91
aleksandr.polyakov <alexworkaccent@gmail.com>
2019-09-05 20:09:29 +0300

    Add UserLoginException exception

---
+++ flowershop/src/main/java/com/accenture/flowershop/exception/UserLoginException.java
@@ -0,0 +1,11 @@
+package com.accenture.flowershop.exception;
+
+public class UserLoginException extends Exception {
+
+    public UserLoginException() {
+    }
+
+    public UserLoginException(String message) {
+        super(message);
+    }
+}
