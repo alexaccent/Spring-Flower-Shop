dc11212e909f513553bdbdf78b86788c3cc6d897
aleksandr.polyakov <alexworkaccent@gmail.com>
2019-09-16 17:56:13 +0300

    Edit main.js

--- flowershop/src/main/webapp/static/js/main.js
+++ flowershop/src/main/webapp/static/js/main.js
@@ -17,41 +17,14 @@ $(document).ready(function() {
 
                     $("#login").addClass("red-input");
                     $("#button-reg").prop("disabled", true);
-                    console.log("Логин уже занят");
+//                    alert("Логин уже занят");
                 } else {
 
                     $("#login").removeClass("red-input");
                     $("#button-reg").prop("disabled", false);
-                    console.log("Логин свободен");
+//                    alert("Логин свободен");
                 }
             },
         });
     });
-
-    function ajaxForRest(login) {
-        var json;
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:8080/rest/checkeduser/",
-            data: login,
-            contentType: false,
-            cache: false,
-            processData: false,
-            success: function (result) {
-                json = jQuery.parseJSON(result);
-
-                if (json) {
-
-                    $("#fieldset").attr("disabled");
-                    $("#login").css('box-shadow', '0 0 0 0.2rem rgba(232, 4, 36, 0.25) !important');
-                    $("#login").css("background-color", "aqua");
-
-                    console.log("Пользователь уже существет");
-                } else {
-                    console.log("Пользователь не найден");
-                }
-            },
-        });
-    }
-
 });
